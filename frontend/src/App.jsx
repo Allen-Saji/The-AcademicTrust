56610bca86d21a8cc1a3bbd4f4407f616b1762e3
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-04-14 02:26:02 -0700

    Studentpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Header from './Pages/Homepage/Header'
 import LoginPage from './Pages/Login/LoginPage';
+import About from './Pages/About/About';
+import StudentHeader from './Pages/StudentLogin/StudentHeader';
 
 function App() {
  
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Header />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/studentlogin' element={<StudentHeader />} />
       </Routes>
     </Fragment>
   );
---
+++ frontend/src/Pages/About/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import classes from './about.module.css';
+
+const About = ()=>{
+    return (
+      <React.Fragment>
+     
+        <div className={classes.mainaboutpage}>
+          <div className={classes.sidebar}>
+            <p>hhmm</p>
+          </div>
+          <div className={classes.about}>
+            <div className={classes.aboutname}>
+              <h3>About</h3>
+            </div>
+            <div className={classes.aboutdescription}>
+              befkjwbef wefjbwkef wekjjthkwetkjewtm ewtkhjew wthwithtw jherwejh
+              kjerwelrh kuyriwutiuwe lihlkrhwlhwjrh wyeruyi uhr uer uhiuh
+              kjbdjbjwef hoihqwh kjhohwf lihwqwhf oihwoiffhw hoowhfom,n
+              hkhwhqkwuh iugiwg iugiwwgiqwg iugqw igqwgiyg fuyfyfuwf
+              igwiufuwwvduw dygvdqywgdvyv giugigihg guygwr wurriuy ygirgiwb
+              asjfijhdsfkyi iuiuhfhiuefh iuhuihf iuihwefiuhiua iuhiuwehe
+              iuhiuhef iu huiwef.uygwy ygwwyf igw ugf iugwf uhewiufhwe iuhsifh a
+              iuhf iw fuhwf iuef ewfiug jbejhe wieuwiheuhw iuwehfiwu ewi iwueh
+              we iwwue iuweeh iuwerh iwuuee weiu iuegt wegtiuwe iugeieu tweit
+              iugweti wieutg iugewt iuwegt iweget iuwegt iuqgweitug iwetgiw
+              tiugt iugt w iuge gwitugqwi t iuget iwgtiugetiu iugiweutg
+              iewutgiuqge htowehtowhet iuth ugetiqueg t. ikgdifugeiufygweuify
+              8wegefgwefyw iwweeyfgwiy fuwf iewgyfiywegf w iwgf wefuy ywgwufygw
+              euf iwgwefi ig hohohjfhjfhdjskfgigfjw jhkfjh iuefi iwuef uewf
+              ieuefhwihufh iuuwffhwiuhfw ityutr iugh i,nbskjasnk oiiwwhdwqd h
+              owhwduw d uwdh iuwhd widuwh iuqwhd wqudh iwud idug ug . iugqye2
+              beugqw 8wge w ige iuge egiygeyugwd
+              uqgwywqoiuiwugqwy98y9uehwjhdqw87tdhdgshdvmhwvyuwr9yuwvhvwjdvqw
+              iuqgwiurg quwyrg 8yqgqw iyqgqwuyqwg wuygq wru iyg 8w7rt iuigq7wg
+              wq igiqwudgqwiwdg8qw wdg 8qwgqiwugdqw 8ddygqw 8wgd8qwgdq dgq8wgd.
+              uyqgwru qwyugqwwygqwu
+              gyqwgiqhwgruywqgr7ygwurqgywigqwiwgdqwoiuyeweiuhjkwbkjwbjwkjwerhw
+              uwqhri wq iquwgri qwirgrqw riqwurgr qwirg wquywqfruyrg qwriiqgw
+              iuqhwiwuhq wiqwgryqgw rouyqgwrougqw gqwroyqgowryurgwq r uygwrqi
+              wrygqw r ywgyuq wguryqgw ruqw gqwrug qowu oqwurgro
+              qrqiuwgoqrygoiugrrjbwkbdais wuyihwjfnwkjfn iwuhwiuhw iuwqhiuwhiuhw
+              iuwhiwuhuwhf iuwfhiuwf iqwgfiuqw fuywgfi fygyuqwyfg uqwygf
+              qwgfqyqwgf iwygfi wfguyqgw fuwygfuyg wfyqwgfuyg wfug wfgyuygu wf
+              uywgfu wfuy gwfuu uwyfgua w fguywgf ugwfuygwuf uywgf iuwgf
+              iwygfiywgfiugweiff ywgwi wfgwgf f f wefjwnfwkj iwufh fuwigf wiuufg
+              iwugfi iqugw figw fi iuwgfi wugf iwgfifugigwuegfi gwef wuegffi
+              uwefgiuwgfi gef iefiugeif ugeff iugifug ifg iwufg wifugui ufuw
+              iugf wofhi. wfigweifgwfi iuqwgfigwifgwe iuwfhiwf iuwf ihuwfu iwhef
+              eweufh eufh eufhi fuh fiuef iuwgfiu wei geiefuiwugf iuw igf
+              eiwfgie iugf igf i iefugiwegfiwf iwfuwgfi wfg iuwgf gieuegfieugfi
+              iewugfig weffiugwe owufhiwgf gfwg efuygwyf wuwfiuugewif
+            </div>
+            <div className={classes.aboutname}>
+              <h3>Objectives</h3>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+};
+export default About;
