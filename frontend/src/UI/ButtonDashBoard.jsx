f2ba1152897ce1ec4758b61825f512c56b51b62a
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-04-14 02:25:46 -0700

    StudentPage

--- frontend/src/UI/ButtonDashBoard.jsx
+++ frontend/src/UI/ButtonDashBoard.jsx
@@ -4,7 +4,7 @@ import {useNavigate } from "react-router-dom";
 const ButtonDashBoard = (props)=>{ 
   const navigate = useNavigate()
   function clickHandler(){
-     console.log("jdjd");
+    
      navigate(props.routing);
   }
     return(
