023bfa08e02ad808eae7d1d16762e1fc6b7e2a29
Allen Saji <allensaji04@gmail.com>
2023-05-19 09:47:51 +0530

    add authentication and react redux toolkit

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -98,7 +98,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 module.exports = {
