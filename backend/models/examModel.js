eea8b61436546a85e7e4ee514b9f597c7d514c2c
Allen Saji <allensaji04@gmail.com>
2023-05-22 15:42:55 +0530

    change examSchema

--- backend/models/examModel.js
+++ backend/models/examModel.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
 const examSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
   date: {
     type: Date,
     required: true,
