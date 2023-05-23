9ca3c42377c9bb70c25af994b21538ed23b5a2d9
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-05-23 00:16:06 -0700

    changed all headers to same

--- frontend/src/Pages/Login/EmployerLogin/EmployerLoginHeader.jsx
+++ frontend/src/Pages/Login/EmployerLogin/EmployerLoginHeader.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import logo from '../../../assets/logo/ktulogo.png'
 import institution from '../../../assets/logo/employee.svg'
+import logo1 from '../../../assets/logo/gandhilogo.png';
 import classes from "./EmployerLoginPageCss.module.css"
 const EmployerLoginHeader = ()=>{
     return (
       <React.Fragment>
-        <div className={classes.mainLogo}>
-        <img src={logo} height='120px' className={classes.logo} alt="" />
-        </div>
+        <div className={classes.heading}>
+        <img src={logo} height="110px" alt="logo" />
+        <h1 className={classes.mainheading}>
+          APJ ABDUL KALAM TECHNOLOGICAL UNIVERSITY
+        </h1>
+        <img src={logo1} alt="gandhi" height="120px" />
+      </div>
         <div className={classes.logos}>
           <div>
             <img src={institution} width='160px' className={classes.institution} alt="" />
--- frontend/src/Pages/UniversityModule/UniversityHeader.jsx
+++ frontend/src/Pages/UniversityModule/UniversityHeader.jsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import logo from '../../assets/logo/ktulogo.png';
-import classes from './css/UniversityHomepageheader.module.css'
-const UniversityHomepageHeader = ()=>{
-    return(
-        <React.Fragment>
-            <div className={classes.logo}>
-                <img src={logo} alt="Ktu logo" />
-            </div>
-        </React.Fragment>
-    );
+import React from "react";
+import logo from "../../assets/logo/ktulogo.png";
+import classes from "./css/UniversityHomepageheader.module.css";
+import logo1 from "../../assets/logo/gandhilogo.png";
+const UniversityHomepageHeader = () => {
+  return (
+    <React.Fragment>
+      <div className={classes.heading}>
+        <img src={logo} height="110px" alt="logo" />
+        <h1 className={classes.mainheading}>
+          APJ ABDUL KALAM TECHNOLOGICAL UNIVERSITY
+        </h1>
+        <img src={logo1} alt="gandhi" height="120px" />
+      </div>
+    </React.Fragment>
+  );
 };
 export default UniversityHomepageHeader;
