f18cf257bae8a2024426468f507a1195feae9df6
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-05-26 07:53:48 -0700

    commit

--- frontend/src/Pages/StudentModule/StudentHomepageHeader.jsx
+++ frontend/src/Pages/StudentModule/StudentHomepageHeader.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import logo from "../../assets/logo/ktulogo.png";
 import logo1 from '../../assets/logo/gandhilogo.png';
 import classes from "./css/StudentHomepageheader.module.css";
+import { useNavigate } from "react-router-dom";
+
 const StudentHomepageHeader = () => {
+  const Navigate = useNavigate();
+  const logoButtonHanlder = ()=>{
+    Navigate('/');
+  }
   return (
     <React.Fragment>
       <div className={classes.heading}>
-        <img src={logo} height="110px" alt="logo" />
+        <button className="logobutton" onClick={logoButtonHanlder}>
+        <img src={logo}  height="110px" alt="logo" />
+        
+        </button>
+
+       
         <h1 className={classes.mainheading}>
           APJ ABDUL KALAM TECHNOLOGICAL UNIVERSITY
         </h1>
         <img src={logo1} alt="gandhi" height="120px" />
+        
       </div>
     </React.Fragment>
   );
