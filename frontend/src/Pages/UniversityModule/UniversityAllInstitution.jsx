6f474864b33024b2d2c78392e48920ac7980a9f0
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-05-28 10:24:43 -0700

    commit

--- frontend/src/Pages/UniversityModule/UniversityAllInstitution.jsx
+++ frontend/src/Pages/UniversityModule/UniversityAllInstitution.jsx
@@ -3,13 +3,26 @@ import { Fragment } from "react";
 import classes from "./css/UniversityHomepageBody.module.css";
 import "./css/styles.css";
 import { useState } from "react";
-
+import searchbutton from '../../assets/searchbutton.png';
+import editpencil from '../../assets/editpencilbutton.png';
+import saveTick from '../../assets/saveTick.png';
+import cancelSymbol from '../../assets/cancelSaveButton.png';
+import Modal from "../../UI/Modal";
+import deletelogo from '../../assets/deleteButton.png';
 const UniversityAllInstitution = () => {
+
+  const [closeSubmitOverlay,setCloseSubmitOverlay] = useState(false);
+
   const [allInstitutionButton, setallInstitutionButton] = useState(true);
   const [addInstitutionButton, setaddInstitutionButton] = useState(false);
   const [updateInstitutionButton, setupdateInstitutionButton] = useState(false);
   const [deleteInstitutionButton, setdeleteInstitutionButton] = useState(false);
 
+  const [updateInstitutionHtmlFlag,setUpdateInstitutionHtmlFlag] = useState(true);
+  const [updateEachInstitutionButton,setUpdateEachInstitutionButton] = useState(false);
+  const [saveUpdatedCollege,setSaveUpdatedCollege] = useState(false);
+
+  const [showDeleteDialogeBox,setShowDeleteDialogeBox] = useState(false);
   const allInstitutionButtonHandler = () => {
     setallInstitutionButton(true);
     setaddInstitutionButton(false);
@@ -35,6 +48,39 @@ const UniversityAllInstitution = () => {
     setdeleteInstitutionButton(true);
   };
 
+  const updateEachInstitutionButtonHandler = ()=>{
+    setUpdateEachInstitutionButton(true);
+    setUpdateInstitutionHtmlFlag(false);
+  }
+  const updateInstitutionFormCancelButtonHandler = ()=>{
+    setUpdateInstitutionHtmlFlag(true);
+    setUpdateEachInstitutionButton(false);
+  }
+  const updateInstitutionFormSaveButtonHandler =()=>{
+    setUpdateInstitutionHtmlFlag(true);
+    setUpdateEachInstitutionButton(false);
+    setSaveUpdatedCollege(true);
+    setCloseSubmitOverlay(true);
+  };
+  const onCloseSubmit =()=>{
+    setCloseSubmitOverlay(false);
+}
+const updateInstitutionSaveChangesClickHandler =()=>{
+  setCloseSubmitOverlay(false);
+}
+
+const deleteEachInstitutionButtonHandler =()=>{
+  setShowDeleteDialogeBox(true);
+
+}
+const deleteInstitutionSaveChangesClickHandler =()=>{
+  setShowDeleteDialogeBox(false);
+}
+
+const deleteInstitutionSaveChangesCancelClickHandler = ()=>{
+  setShowDeleteDialogeBox(false);
+}
+
   const allInstitutionStyle = allInstitutionButton
     ? "sidebarActive"
     : "sidebarNotActive";
@@ -47,6 +93,144 @@ const UniversityAllInstitution = () => {
   const deleteInstitutionStyle = deleteInstitutionButton
     ? "sidebarActive"
     : "sidebarNotActive";
+
+
+    const allInstitutionNamesTable = (<div className="maintableforinstitution">
+    <table className="institutiontable">
+      <tr>
+        <th>All Institution</th>
+        <th>Course Code</th>
+      </tr>
+      <tr>
+        <td>St Josephs College</td>
+        <td>SJC0001P</td>
+      </tr>
+      <tr>
+        <td>Aimil Bij University</td>
+        <td>Aimu999</td>
+      </tr>
+      <tr>
+        <td>College of engineering TVM</td>
+        <td>CET5542</td>
+      </tr>
+      <td>College of Kidangoor</td>
+      <td>CEK78945</td>
+    </table>
+  </div>);
+    const addInstitutionHtml = (<div className="maintableforaddinstitution">
+    <h3 className="addinstitutionheading">Add new institution to the university</h3>
+<form className="addinstitutionform">
+    <div className="formdivs">
+    <label htmlFor="nameinput">Name:</label>
+    <input className="institutionnameinput" id="nameinput" type="text" />
+    </div>
+    <div className="formdivs">
+    <label htmlFor="">Address:</label>
+    <input className="institutionaddressinput" type="text" />
+    </div>
+    <div className="formdivs">
+    <label htmlFor="">Email:</label>
+    <input className="institutionemailinput" type="text" />
+    </div>
+    <div className="formdivs">
+    <label htmlFor="">Contact No:</label>
+    <input className="institutioncontactnoinput" type="text" />
+    </div>
+    <div className="formdivs">
+    <label htmlFor="">College Id:</label>
+    <input className="institutioncollegeidinput" type="text" />
+    </div>
+    <div className="formdivs">
+    <label htmlFor="">Year of Reg:</label>
+    <input className="institutionyearofreginput" type="text" />
+    </div>
+    <button className="addinstitutionbutton" type="submit">Save</button>
+</form>
+</div>);
+
+    const mainUpdateInstitution = (
+      <div className="maininstitution">
+        <div className="maininstitutionsearchinputbutton">
+          <input className="maininstitutionsearchinput" type="text" placeholder="search" />
+          <button className="maininstitutionsearchbutton"><img className="searchbuttonimg" src={searchbutton} alt="" /></button>
+        </div>
+        <div className="allinstitutiontext"><p>All Institutions</p></div>
+        <div className="spacebetween"></div>
+        <div className="updatecollegemainbutton">
+          <button onClick={updateEachInstitutionButtonHandler} className="updatecollegemainbuttonname">Aimil Bij Joseph College of engineering</button>
+          <button onClick={updateEachInstitutionButtonHandler} className="updatecollegemainbuttonsymbol"><img className="editpencilbuttonimg" src={editpencil} alt="" /></button>
+        </div>
+      </div>
+    );
+      const updateInstitutionForm =(
+        <div className="maintableforupdateinstitution">
+            <h3 className="addinstitutionheading">Update institution to the university</h3>
+        <form className="updateinstitutionform">
+            <div className="formdivs">
+            <label htmlFor="nameinput">Name:</label>
+            <input className="institutionnameinput" id="nameinput" type="text" />
+            </div>
+            <div className="formdivs">
+            <label htmlFor="">Address:</label>
+            <input className="institutionaddressinput" type="text" />
+            </div>
+            <div className="formdivs">
+            <label htmlFor="">Email:</label>
+            <input className="institutionemailinput" type="text" />
+            </div>
+            <div className="formdivs">
+            <label htmlFor="">Contact No:</label>
+            <input className="institutioncontactnoinput" type="text" />
+            </div>
+            <div className="formdivs">
+            <label htmlFor="">College Id:</label>
+            <input className="institutioncollegeidinput" type="text" />
+            </div>
+            <div className="formdivs">
+            <label htmlFor="">Year of Reg:</label>
+            <input className="institutionyearofreginput" type="text" />
+            </div>
+            <button onClick={updateInstitutionFormSaveButtonHandler} className="addinstitutionbutton" type="submit">Save</button>
+            <button onClick={updateInstitutionFormCancelButtonHandler} className="addinstitutioncancelbutton" type="submit">Cancel</button>
+
+        </form>
+      </div>
+      );
+      const saveChangesHtml = (
+        <Modal onCloseSubmit={onCloseSubmit}>
+        <div className="savechangesmainwindow">
+          <div className="savechangestext"><p>Save Changes ?</p></div>
+          <div>
+            <button onClick={updateInstitutionSaveChangesClickHandler} className="savechangestickbutton"><img className="savechangesticksymbol" src={saveTick} alt="" /></button>
+            <button onClick={updateInstitutionSaveChangesClickHandler} className="savechangescancelbutton"><img className="savechangescancelsymbol" src={cancelSymbol} alt="" /></button>
+          </div>
+        </div>
+        </Modal>
+      );
+
+      const mainUpdateDeleteInstitution = (
+        <div className="maininstitution">
+          <div className="maininstitutionsearchinputbutton">
+            <input className="maininstitutionsearchinput" type="text" placeholder="search" />
+            <button className="maininstitutionsearchbutton"><img className="searchbuttonimg" src={searchbutton} alt="" /></button>
+          </div>
+          <div className="allinstitutiontext"><p>All Institutions</p></div>
+          <div className="spacebetween"></div>
+          <div className="updatecollegemainbutton">
+            <button onClick={deleteEachInstitutionButtonHandler} className="updatecollegemainbuttonname">Aimil Bij Joseph College of engineering</button>
+            <button onClick={deleteEachInstitutionButtonHandler} className="updatecollegemainbuttonsymbol"><img className="editpencilbuttonimg" src={deletelogo} alt="" /></button>
+          </div>
+        </div>
+      );
+        const deleteInstitutionConfirmBox = (<Modal>
+          <div className="savechangesmainwindow">
+            <div className="savechangestext"><p>Are you sure want delete ?</p></div>
+            <div>
+              <button onClick={deleteInstitutionSaveChangesClickHandler} className="savechangestickbutton"><img className="savechangesticksymbol" src={saveTick} alt="" /></button>
+              <button onClick={deleteInstitutionSaveChangesCancelClickHandler} className="savechangescancelbutton"><img className="savechangescancelsymbol" src={cancelSymbol} alt="" /></button>
+            </div>
+          </div>
+          </Modal>);
 
   return (
     <Fragment>
@@ -88,62 +272,13 @@ const UniversityAllInstitution = () => {
         </div>
         <div className={classes.spacebetween1}></div>
       </div>
-      {allInstitutionButton && (
-        <div className="maintableforinstitution">
-          <table className="institutiontable">
-            <tr>
-              <th>All Institution</th>
-              <th>Course Code</th>
-            </tr>
-            <tr>
-              <td>St Josephs College</td>
-              <td>SJC0001P</td>
-            </tr>
-            <tr>
-              <td>Aimil Bij University</td>
-              <td>Aimu999</td>
-            </tr>
-            <tr>
-              <td>College of engineering TVM</td>
-              <td>CET5542</td>
-            </tr>
-            <td>College of Kidangoor</td>
-            <td>CEK78945</td>
-          </table>
-        </div>
-      )}
-      {addInstitutionButton && (
-        <div className="maintableforaddinstitution">
-            <h3 className="addinstitutionheading">Add new institution to the university</h3>
-        <form className="addinstitutionform">
-            <div className="formdivs">
-            <label htmlFor="nameinput">Name:</label>
-            <input className="institutionnameinput" id="nameinput" type="text" />
-            </div>
-            <div className="formdivs">
-            <label htmlFor="">Address:</label>
-            <input className="institutionaddressinput" type="text" />
-            </div>
-            <div className="formdivs">
-            <label htmlFor="">Email:</label>
-            <input className="institutionemailinput" type="text" />
-            </div>
-            <div className="formdivs">
-            <label htmlFor="">Contact No:</label>
-            <input className="institutioncontactnoinput" type="text" />
-            </div>
-            <div className="formdivs">
-            <label htmlFor="">College Id:</label>
-            <input className="institutioncollegeidinput" type="text" />
-            </div>
-            <div className="formdivs">
-            <label htmlFor="">Year of Reg:</label>
-            <input className="institutionyearofreginput" type="text" />
-            </div>
-            <button className="addinstitutionbutton" type="submit">Save</button>
-        </form>
-      </div>
-      )}
+      {allInstitutionButton && allInstitutionNamesTable}
+      {addInstitutionButton && addInstitutionHtml}
+      {updateInstitutionButton && updateInstitutionHtmlFlag && mainUpdateInstitution}
+      {updateEachInstitutionButton && updateInstitutionForm}
+      {closeSubmitOverlay && saveChangesHtml}
+      {deleteInstitutionButton && mainUpdateDeleteInstitution}
+      {showDeleteDialogeBox && deleteInstitutionConfirmBox}
     </Fragment>
   );
 };
--- frontend/src/Pages/UniversityModule/UniversityCourse.jsx
+++ frontend/src/Pages/UniversityModule/UniversityCourse.jsx
@@ -2,10 +2,21 @@ import React, { Fragment } from "react";
 import { useState } from "react";
 import "./css/styles.css";
 import classes from "./css/UniversityHomepageBody.module.css";
+import searchbutton from "../../assets/searchbutton.png";
+import editpencil from '../../assets/editpencilbutton.png';
+import saveTick from '../../assets/saveTick.png';
+import cancelSymbol from '../../assets/cancelSaveButton.png';
+import Modal from "../../UI/Modal";
+import deletelogo from "../../assets/deleteButton.png";
 const UniversityCourse = () => {
   const [addCourseButton, setAddCourseButton] = useState(true);
   const [updateCourseButton, setUpdateCourseButton] = useState(false);
   const [deleteCourseButton, setDeleteCourseButton] = useState(false);
+
+  const [updateEachCourses, setUpdateEchCourses] = useState(false);
+  const [updateEachCoursesFlag,setUpdateEachCoursesFlag] = useState(true);
+  const [updateEachCoursesConfirmBoxFlag,setUpdateEachCoursesConfirmBoxFlag] = useState(false);
+  const [updateDeleteCoursesConfirmBoxFlag,setDeleteEachCoursesConfirmBoxFlag] = useState(false);
 
   const addCourseButtonHandler = () => {
     setAddCourseButton(true);
@@ -22,6 +33,44 @@ const UniversityCourse = () => {
     setUpdateCourseButton(false);
     setDeleteCourseButton(true);
   };
+
+  const addCoursesFormSaveButtonHandler = () => {};
+  const addCoursesFormCancelButtonHandler = () => {};
+  const updateEachInstitutionButtonHandler = () => {
+    setUpdateEchCourses(true);
+    setUpdateEachCoursesFlag(false);
+  };
+  const cancelEachCoursesButtonHandler =()=>{
+    setUpdateEchCourses(false);
+    setUpdateEachCoursesFlag(true);
+  }
+  const saveEachCoursesButtonHandler = ()=>{
+    setUpdateEachCoursesFlag(true);
+    setUpdateEchCourses(false);
+    setUpdateEachCoursesConfirmBoxFlag(true);
+  }
+
+  const updateCoursesConfirmBoxTickClickHandler = ()=>{
+    setUpdateEachCoursesFlag(true);
+    setUpdateEchCourses(false);
+    setUpdateEachCoursesConfirmBoxFlag(false);
+
+  }
+  const updateCoursesConfirmBoxCancelClickHandler=()=>{
+    setUpdateEachCoursesConfirmBoxFlag(false);
+  }
+  const deleteEachCoursesButtonHandler =()=>{
+    setDeleteEachCoursesConfirmBoxFlag(true);
+  };
+
+  const deleteCoursesConfirmBoxTickClickHandler =()=>{
+    setDeleteEachCoursesConfirmBoxFlag(false);
+
+  };
+  const deleteCoursesConfirmBoxCancelClickHandler=()=>{
+    setDeleteEachCoursesConfirmBoxFlag(false);
+  }
+
   const addCourseStyle = addCourseButton ? "sidebarActive" : "sidebarNotActive";
   const updateCourseStyle = updateCourseButton
     ? "sidebarActive"
@@ -29,6 +78,153 @@ const UniversityCourse = () => {
   const deleteCourseStyle = deleteCourseButton
     ? "sidebarActive"
     : "sidebarNotActive";
+
+  const addCourseMainWindow = (
+    <div className="addCoursesMainLogin">
+      <div className="addCoursesText">
+        <h3>Add new Courses</h3>
+      </div>
+      <form className="updateinstitutionform">
+        <div className="formdivs">
+          <label htmlFor="nameinput">Name:</label>
+          <input className="coursenameinput" id="nameinput" type="text" />
+        </div>
+        <div className="formdivs">
+          <label htmlFor="">Course Id:</label>
+          <input className="courseidinput" type="text" />
+        </div>
+        <div className="formdivs">
+          <label htmlFor="">Credits:</label>
+          <input className="creditcourseinput" type="text" />
+        </div>
+        <button
+          onClick={addCoursesFormSaveButtonHandler}
+          className="addinstitutionbutton"
+          type="submit"
+        >
+          Save
+        </button>
+        <button
+          onClick={addCoursesFormCancelButtonHandler}
+          className="addinstitutioncancelbutton"
+          type="submit"
+        >
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+  const updateCoursesWindow = (
+    <div className="maincourses">
+      <div className="maininstitutionsearchinputbutton">
+        <input
+          className="maininstitutionsearchinput"
+          type="text"
+          placeholder="search"
+        />
+        <button className="maininstitutionsearchbutton">
+          <img className="searchbuttonimg" src={searchbutton} alt="" />
+        </button>
+      </div>
+      <div className="allinstitutiontext">
+        <p>All Courses</p>
+      </div>
+      <div className="spacebetween"></div>
+      <div className="updatecollegemainbutton">
+        <button
+          onClick={updateEachInstitutionButtonHandler}
+          className="updatecollegemainbuttonname"
+        >
+          Data Structures
+        </button>
+        <button
+          onClick={updateEachInstitutionButtonHandler}
+          className="updatecollegemainbuttonsymbol"
+        >
+          <img className="editpencilbuttonimg" src={editpencil} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+
+  const updateEachCoursesWindow = (
+    <div className="addCoursesMainLogin">
+      <h3 className="addCoursesText">Update Courses to the university</h3>
+      <form className="updateinstitutionform">
+        <div className="formdivs">
+          <label htmlFor="nameinput">Name:</label>
+          <input className="coursenameinput" id="nameinput" type="text" />
+        </div>
+        <div className="formdivs">
+          <label htmlFor="">Course Id:</label>
+          <input className="courseidinput" type="text" />
+        </div>
+        <div className="formdivs">
+          <label htmlFor="">Credits:</label>
+          <input className="creditcourseinput" type="text" />
+        </div>
+
+        <button onClick={saveEachCoursesButtonHandler} className="addinstitutionbutton" type="submit">
+          Save
+        </button>
+        <button onClick={cancelEachCoursesButtonHandler} className="addinstitutioncancelbutton" type="submit">
+          Cancel
+        </button>
+      </form>
+    </div>
+  );
+  const updateEachCoursesConfirmBox = (<Modal>
+    <div className="savechangesmainwindow">
+      <div className="savechangestext"><p>Save Changes ?</p></div>
+      <div>
+        <button onClick={updateCoursesConfirmBoxTickClickHandler} className="savechangestickbutton"><img className="savechangesticksymbol" src={saveTick} alt="" /></button>
+        <button onClick={updateCoursesConfirmBoxCancelClickHandler} className="savechangescancelbutton"><img className="savechangescancelsymbol" src={cancelSymbol} alt="" /></button>
+      </div>
+    </div>
+    </Modal>);
+
+const deleteEachCourseWindow = (
+  <div className="maincourses">
+    <div className="maininstitutionsearchinputbutton">
+      <input
+        className="maininstitutionsearchinput"
+        type="text"
+        placeholder="search"
+      />
+      <button className="maininstitutionsearchbutton">
+        <img className="searchbuttonimg" src={searchbutton} alt="" />
+      </button>
+    </div>
+    <div className="allinstitutiontext">
+      <p>All Courses</p>
+    </div>
+    <div className="spacebetween"></div>
+    <div className="updatecollegemainbutton">
+      <button
+        onClick={deleteEachCoursesButtonHandler}
+        className="updatecollegemainbuttonname"
+      >
+        Data Structures
+      </button>
+      <button
+        onClick={deleteEachCoursesButtonHandler}
+        className="updatecollegemainbuttonsymbol"
+      >
+        <img className="editpencilbuttonimg" src={deletelogo} alt="" />
+      </button>
+    </div>
+  </div>
+);
+const deleteEachCoursesConfirmBoxWindow = (<Modal>
+  <div className="savechangesmainwindow">
+    <div className="savechangestext"><p>Save Changes ?</p></div>
+    <div>
+      <button onClick={deleteCoursesConfirmBoxTickClickHandler} className="savechangestickbutton"><img className="savechangesticksymbol" src={saveTick} alt="" /></button>
+      <button onClick={deleteCoursesConfirmBoxCancelClickHandler} className="savechangescancelbutton"><img className="savechangescancelsymbol" src={cancelSymbol} alt="" /></button>
+    </div>
+  </div>
+  </Modal>);
+
   return (
     <Fragment>
       <div className={classes.universityhomepagesidebar}>
@@ -56,6 +252,12 @@ const UniversityCourse = () => {
           </button>
         </div>
         <div className={classes.spacebetween1}></div>
+        {addCourseButton && addCourseMainWindow}
+        {updateCourseButton && updateEachCoursesFlag && updateCoursesWindow}
+        {updateEachCourses && updateEachCoursesWindow}
+        {updateEachCoursesConfirmBoxFlag && updateEachCoursesConfirmBox}
+        {deleteCourseButton && deleteEachCourseWindow}
+        {updateDeleteCoursesConfirmBoxFlag && deleteEachCoursesConfirmBoxWindow}
       </div>
     </Fragment>
   );
