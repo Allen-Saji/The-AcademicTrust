84302541fd9eff0bb976f8b9df3956db4f267af8
Allen Saji <allensaji04@gmail.com>
2023-06-05 19:59:19 +0530

    add course state

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
 import resultReducer from "../features/result/resultSlice";
 import institutionReducer from "../features/institution/institutionSlice";
+import courseReducer from "../features/course/courseSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     result: resultReducer,
     institution: institutionReducer,
+    course: courseReducer,
   },
 });
