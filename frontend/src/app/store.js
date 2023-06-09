00d66466949a4c220a93566c211bd3befbc16d36
Allen Saji <allensaji04@gmail.com>
2023-06-09 18:34:22 +0530

    remove result state

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,13 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import resultReducer from "../features/result/resultSlice";
 import institutionReducer from "../features/institution/institutionSlice";
 import courseReducer from "../features/course/courseSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    results: resultReducer,
     institutions: institutionReducer,
     courses: courseReducer,
   },
