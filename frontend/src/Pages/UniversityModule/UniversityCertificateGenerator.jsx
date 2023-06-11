57ae879eb53ed0ea640bfeff657aa09659a7ce70
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-06-11 07:20:35 -0700

    commit

--- frontend/src/Pages/UniversityModule/UniversityCertificateGenerator.jsx
+++ frontend/src/Pages/UniversityModule/UniversityCertificateGenerator.jsx
@@ -3,15 +3,28 @@ import classes from "./css/UniversityHomepageBody.module.css";
 import "./css/styles.css";
 const UniversityCertificateGenerator = () => {
   const [viewMesg,setMesg] = useState(false);
+  const [selectedBatchYear,setSelectedBatchYear] = useState(2019);
+
   const generateButtonHandler = ()=>{
     setMesg(true);
+ 
+  }
+  const onChangeSelectorHandler =(event)=>{
+    setSelectedBatchYear(event.target.value);
   }
   return (
     <Fragment>
       <div className='certificateSelectorbody'>
+        <div className="dropdownbox">
+        <select onChange={onChangeSelectorHandler} className="dropdownselector">
+          <option value={2019}>2019</option>
+          <option value={2024}>2024</option>
+          <option value={2029}>2029</option>
+        </select>
+        </div>
+     
         <button className="generatebutton" onClick={generateButtonHandler}>Generate</button>
-        <button className="generatebutton">Deploy</button>
-       {viewMesg && <p className="generatedtext">Generated successfully!!</p>}      </div>
+       {viewMesg && <p className="generatedtext"> Batch Generated successfully!!</p>}      </div>
     </Fragment>
   );
 };
