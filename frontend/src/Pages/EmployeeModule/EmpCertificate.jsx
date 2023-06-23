9cd4aed6734b16310b4d7dc35e64802298845b56
Krishnaprasadcp <krishnaprasad61016@gmail.com>
2023-06-23 10:49:38 -0700

    commit

--- frontend/src/Pages/EmployeeModule/EmpCertificate.jsx
+++ frontend/src/Pages/EmployeeModule/EmpCertificate.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../assets/logo/ktulogo.png";
 import "./css/certificate.css";
 const EmpCertificate = () => {
