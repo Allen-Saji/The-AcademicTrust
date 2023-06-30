15eee84df4bd46a3728b645c88d984ea0b011cb8
Allen Saji <allensaji04@gmail.com>
2023-06-30 23:25:58 +0530

    add certificate error handling

--- frontend/src/Pages/UniversityModule/UniversityCertificateGenerator.jsx
+++ frontend/src/Pages/UniversityModule/UniversityCertificateGenerator.jsx
@@ -1,13 +1,12 @@
 import React, { Fragment, useState } from "react";
-import classes from "./css/UniversityHomepageBody.module.css";
+import { toast } from "react-toastify";
+import Spinner from "../../Components/Spinner";
 
 import "./css/styles.css";
 import axios from "axios";
 const UniversityCertificateGenerator = () => {
-  const [viewMesg, setMesg] = useState(false);
-  const[errorMessage,setErrorMessage] = useState('');
   const [selectedBatchYear, setSelectedBatchYear] = useState(2018);
-
+  const [loading, setLoading] = useState(false);
   const API_URL1 =
     "http://localhost:5000/api/admin/certificate/getCertificateDetails";
   const API_URL2 = "http://localhost:5000/api/admin/certificate";
@@ -15,50 +14,50 @@ const UniversityCertificateGenerator = () => {
     "http://localhost:5000/api/admin/certificate/issueCertificate";
 
   const issueCertificate = async (data) => {
-    const response = await axios.post(API_URL2, data);
-    if (response.status === 200) {
-      console.log(response.data);
-      const data2 = { students: response.data };
-      const certificateDetails = await axios.post(API_URL1, data2);
-      // console.log(certificateDetails.data.length);
-      if (certificateDetails.status === 200) {
-        for (let i = 0; i < certificateDetails.data.length; i++) {
-          const data3 = certificateDetails.data[i];
-          const data4 = {
-            registerNumber: data3.registration_no,
-            studentName: data3.student_name,
-            institution: data3.institution,
-            yearOfAdmission: data3.year_of_adm,
-            monthAndYearOfPassing: data3.monthAndYearOfPassing,
-            cgpa: data3.cgpa,
-            subjectNames: data3.subjectNamesBySemester,
-            subjectCredits: data3.subjectCreditsBySemester,
-            subjectGrades: data3.subjectGradesBySemester,
-            subjectExamMonths: data3.subjectExamMonthsBySemester,
-            subjectExamYears: data3.subjectExamYearsBySemester,
-          };
-          const reply = await axios.post(API_URL3, data4);
-          if (response.status === 200) {
-            console.log(reply.data);
+    try {
+      const response = await axios.post(API_URL2, data);
+      if (response.status === 200) {
+        const data2 = { students: response.data };
+        const certificateDetails = await axios.post(API_URL1, data2);
+        if (certificateDetails.status === 200) {
+          for (let i = 0; i < certificateDetails.data.length; i++) {
+            const data3 = certificateDetails.data[i];
+            const data4 = {
+              registerNumber: data3.registration_no,
+              studentName: data3.student_name,
+              institution: data3.institution,
+              yearOfAdmission: data3.year_of_adm,
+              monthAndYearOfPassing: data3.monthAndYearOfPassing,
+              cgpa: data3.cgpa,
+              subjectNames: data3.subjectNamesBySemester,
+              subjectCredits: data3.subjectCreditsBySemester,
+              subjectGrades: data3.subjectGradesBySemester,
+              subjectExamMonths: data3.subjectExamMonthsBySemester,
+              subjectExamYears: data3.subjectExamYearsBySemester,
+            };
+            const reply = await axios.post(API_URL3, data4);
+            if (reply.status === 200) {
+              console.log(reply.data);
+              toast.success(`Certificate issued successfully!`);
+            }
           }
-  
         }
-      } else {
-        throw new Error(response.statusText);
-        setErrorMessage('Certificate already issued')
-
       }
-    } else {
-      throw new Error(response.statusText);
-      setErrorMessage('Certificate already issued')
-
+    } catch (error) {
+      if (error.response && error.response.status === 409) {
+        toast.error("An error occurred");
+      } else {
+        toast.error("An error occurred");
+      }
+    } finally {
+      setLoading(false);
     }
   };
 
-  const generateButtonHandler = () => {
+  const generateButtonHandler = async () => {
     const data = { year_of_adm: selectedBatchYear };
-    issueCertificate(data);
-    setMesg(true);
+    setLoading(true);
+    await issueCertificate(data);
   };
   const onChangeSelectorHandler = (event) => {
     setSelectedBatchYear(event.target.value);
@@ -67,7 +66,7 @@ const UniversityCertificateGenerator = () => {
     <Fragment>
       <div className="certificateSelectorbody">
         <div className="dropdownbox">
-        <label className="batch">Batch</label>
+          <label className="batch">Batch</label>
 
           <select
             onChange={onChangeSelectorHandler}
@@ -79,14 +78,10 @@ const UniversityCertificateGenerator = () => {
           <option value={2029}>2029</option> */}
           </select>
           <button className="generatebutton" onClick={generateButtonHandler}>
-          Generate
-        </button>
+            Generate
+          </button>
         </div>
-       
-        {viewMesg && (
-          <p className="generatedtext"> Batch Generated successfully!!</p>
-        )}{" "}
-        <p>{errorMessage}</p>
+        {loading && <Spinner />}
       </div>
     </Fragment>
   );
